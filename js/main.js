a8db1af0a0707dba8616531ea25257fbf0e2f76a
Nick Hollins <nholl22@wgu.edu>
2021-05-30 11:01:48 -0400

    working with function scopes

--- js/main.js
+++ js/main.js
@@ -10,3 +10,24 @@ function showValue() {
     }
     return userValue;
 }
+
+// lexical scoping 
+const hummus = factor => {
+    const ingredient = function(amount, unit, name) {
+      let ingredientAmount = amount * factor;
+      if (ingredientAmount > 1) {
+        unit += "s";
+      }
+      return console.log(`${ingredientAmount} ${unit} ${name}`);
+    };
+    ingredient(1, "can", "chickpeas");
+    ingredient(0.25, "cup", "tahini");
+    ingredient(0.25, "cup", "lemon juice");
+    ingredient(1, "clove", "garlic");
+    ingredient(2, "tablespoon", "olive oil");
+    ingredient(0.5, "teaspoon", "cumin");
+  };
+
+  console.log(hummus(2));
+
+  
